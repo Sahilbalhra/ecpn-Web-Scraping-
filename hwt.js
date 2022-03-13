22169f545b96c79d4aa3f4d12bb8b0b5c1d7ba92
Sahilbalhra <sahilbalhra0@gmail.com>
2022-03-13 12:39:47 +0530

    Winning team selected

--- hwt.js
+++ hwt.js
@@ -19,13 +19,32 @@ function handleHTML(html) {
   let $ = cheerio.load(html);
   //getting value using css selector in form of array
   let teamArr = $(".match-info.match-info-MATCH .team");
+  //   let teamName;
   // console.log(teamArr);
   for (let i = 0; i < teamArr.length; i++) {
-      //checking the team-gray is present in which class
+    //checking the team-gray is present in which class
     let hasclass = $(teamArr[i]).hasClass("team-gray");
     if (hasclass == false) {
       let teamNameElem = $(teamArr[i]).find(".name");
-      console.log(teamNameElem.text());
+      wTeamName=teamNameElem.text().trim();
+    //   console.log(teamNameElem.text());
+    }
+  }
+  let inningArr = $(".card.content-block.match-scorecard-table>.Collapsible");
+  let htmlStr = "";
+  for (let i = 0; i < inningArr.length; i++) {
+    //   let cHtml=$(inningArr[i]).html();
+    //   htmlStr+=cHtml;
+    //   console.log(htmlStr);
+
+    //team Name
+    let teamNameElem = $(inningArr[i]).find(".header-title.label");
+    let teamName = teamNameElem.text();
+    teamName = teamName.split("INNINGS")[0];
+    teamName = teamName.trim();
+    // console.log(teamName);
+    if(wTeamName==teamName){
+        console.log(teamName);
     }
   }
 }
