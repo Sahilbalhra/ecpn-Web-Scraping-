457d2d06a1b0400e0cb56e16c45285d3734b0c04
Sahilbalhra <sahilbalhra0@gmail.com>
2022-03-15 20:55:10 +0530

    some changes

--- Ibc.js
+++ Ibc.js
@@ -7,23 +7,24 @@ request(
 );
 
 function cb(error, response, html) {
-    if(error){
-
-        console.error("error:", error); // Print the error if one occurred
-    }else{
-        handleHTML(html);
-        // console.log(html);
-    }
+  if (error) {
+    console.error("error:", error); // Print the error if one occurred
+  } else {
+    handleHTML(html);
+    // console.log(html);
+  }
 }
 function handleHTML(html) {
-  let $=cheerio.load(html);
-    let elementArr=$('.d-flex.match-comment-padder.align-items-center .match-comment-long-text');
+  let $ = cheerio.load(html);
+  let elementArr = $(
+    ".d-flex.match-comment-padder.align-items-center .match-comment-long-text"
+  );
   //getting value using css selector in form of array
-  
+
   //conveting to the text
 
-    let text = $(elementArr[0]).text();
-    let htmlData = $(elementArr[0]).html();
-    console.log("Text", text);
-    console.log("HTML Data", htmlData);
+  let text = $(elementArr[0]).text();
+  let htmlData = $(elementArr[0]).html();
+  console.log("Text", text);
+  console.log("HTML Data", htmlData);
 }
--- birthday.js
+++ birthday.js
@@ -18,26 +18,9 @@ function cb(error, response, html) {
 
 function handleHTML(html) {
   let $ = cheerio.load(html);
-  //getting value using css selector in form of array
-  let teamArr = $(".match-info.match-info-MATCH .team");
-  //   let teamName;
-  // console.log(teamArr);
-  for (let i = 0; i < teamArr.length; i++) {
-    //checking the team-gray is present in which class
-    let hasclass = $(teamArr[i]).hasClass("team-gray");
-    if (hasclass == false) {
-      let teamNameElem = $(teamArr[i]).find(".name");
-      wTeamName = teamNameElem.text().trim();
-      //   console.log(teamNameElem.text());
-    }
-  }
   let inningArr = $(".card.content-block.match-scorecard-table>.Collapsible");
   let htmlStr = "";
   for (let i = 0; i < inningArr.length; i++) {
-    //   let cHtml=$(inningArr[i]).html();
-    //   htmlStr+=cHtml;
-    //   console.log(htmlStr);
-
     //team Name
     let teamNameElem = $(inningArr[i]).find(".header-title.label");
     let teamName = teamNameElem.text();
@@ -45,34 +28,17 @@ function handleHTML(html) {
     teamName = teamName.trim();
     // console.log(teamName);
 
-    let hwtName = "";
-    let hwt = 0;
-    if (wTeamName == teamName) {
-      // console.log(teamName);
-      let tableElement = $(inningArr[i]).find(".table.bowler");
-      //getting all bowlers rows
-      let allBowlers = $(tableElement).find("tr");
-      for (let j = 0; j < allBowlers.length; j++) {
-        //getting palyers record in a row through column
-        let allColsOfPlayer = $(allBowlers[j]).find("td");
-        //getting player name
-        let playerName = $(allColsOfPlayer[0]).text();
-        //Number of wickets
-        let wickets = $(allColsOfPlayer[4]).text();
-        // console.log(
-        //   `Winning Team ${wTeamName}  playerName:${playerName}  Wickets:${wickets}`
-        // );
+    //table batman
+    let tableElement = $(inningArr[i]).find(".table.batsman");
+    //getting all bowlers rows
+    let allBowlers = $(tableElement).find("tr");
+    for (let j = 0; j < allBowlers.length; j++) {
+      //getting palyers record in a row through column
+      let allColsOfPlayer = $(allBowlers[j]).find("td");
+      //getting player name
+      let playerName = $(allColsOfPlayer[0]).text();
 
-        if (wickets >= hwt) {
-          hwt = wickets;
-          hwtName = playerName;
-        }
-      }
-      //   console.log(hwtName);
-      //   console.log(hwt);
-      console.log(
-        `Winning Team ${wTeamName}  playerName:${hwtName}  Wickets:${hwt}`
-      );
+      console.log(`Winning Team ${teamName}  playerName:${playerName}`);
     }
   }
 }
